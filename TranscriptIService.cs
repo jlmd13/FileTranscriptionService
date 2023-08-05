4bcea2f2beffea6235292d637e692f9bf99b0cc1
jlmd13 <moradiazjl@gmail.com>
2023-08-05 20:50:34 +0200

    Agregar archivos de proyecto.

---
+++ TranscriptIService.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace FileTranscriptionService
+{
+    [ServiceContract]
+    public interface TranscriptIService
+    {
+        [OperationContract]
+        string[] GetTranscription(FileTranscription fileTranscription);
+    }
+
+    [DataContract]
+    public class FileTranscription
+    {
+        [DataMember]
+        public string FilePath { set; get; }
+
+        [DataMember]
+        public int UserId { set; get; }
+
+        [DataMember]
+        public string Status { set; get; }
+
+        [DataMember]
+        public string Result { set; get; }
+    }
+
+}
---
+++ TranscriptService.svc.cs
@@ -0,0 +1,19 @@
+﻿using DOM.Transcriptions;
+using DOM.TranscriptionService;
+using DOM.TranscriptionService.Enums;
+
+namespace FileTranscriptionService
+{
+    public class TranscriptService : TranscriptIService
+    {
+        public string[] GetTranscription(FileTranscription fileTranscription)
+        {
+            TranscriptionServiceMock transcriptionServiceMock = new TranscriptionServiceMock();
+            TranscriptionResult transcriptionResult = transcriptionServiceMock.TranscriptFile(fileTranscription.FilePath);
+            fileTranscription.Result = transcriptionResult.TranscriptionStatus.Equals(TranscriptionStatusEnum.Ok) ? transcriptionResult.Result : string.Empty;
+
+            return new string[] { transcriptionResult.TranscriptionStatus.ToString(), fileTranscription.Result};
+        }
+    }
+}
+
